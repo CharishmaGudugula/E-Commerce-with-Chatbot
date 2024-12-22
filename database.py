ddee07b5f5da7f4e8b7ee20c3e082188d829e541
Charishma Gudugula <charishma0608@gmail.com>
2024-12-22 14:18:04 +0530

    Initial commit

---
+++ database.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+# Open database
+conn = sqlite3.connect('database.db')
+
+# Create tables
+conn.execute('''CREATE TABLE IF NOT EXISTS users 
+    (userId INTEGER PRIMARY KEY, 
+    password TEXT,
+    email TEXT,
+    firstName TEXT,
+    lastName TEXT,
+    address1 TEXT,
+    address2 TEXT,
+    zipcode TEXT,
+    city TEXT,
+    state TEXT,
+    country TEXT, 
+    phone TEXT
+    )''')
+
+conn.execute('''CREATE TABLE IF NOT EXISTS categories
+    (categoryId INTEGER PRIMARY KEY,
+    name TEXT
+    )''')
+
+conn.execute('''CREATE TABLE IF NOT EXISTS products
+    (productId INTEGER PRIMARY KEY,
+    name TEXT,
+    price REAL,
+    description TEXT,
+    image TEXT,
+    stock INTEGER,
+    categoryId INTEGER,
+    FOREIGN KEY(categoryId) REFERENCES categories(categoryId)
+    )''')
+
+conn.execute('''CREATE TABLE IF NOT EXISTS kart
+    (userId INTEGER,
+    productId INTEGER,
+    FOREIGN KEY(userId) REFERENCES users(userId),
+    FOREIGN KEY(productId) REFERENCES products(productId)
+    )''')
+
+# Close connection
+conn.close()
---
+++ test_nltk.py
@@ -0,0 +1,3 @@
+import nltk
+print(nltk.__version__)  # This will print the version of NLTK installed
+
